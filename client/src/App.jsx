382521cd7b032b8523810dcc8dc58e0224ad82d2
cyrus fullam <cyrusfullam@gmail.com>
2023-07-18 16:52:52 -0400

    steam openID added

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+function test() {
+    
+  }
+  return (
+    <>
+    <a id='steam-login' href="http://localhost:4000/api/auth/steam" onClick={test}><img src="https://community.cloudflare.steamstatic.com/public/shared/images/signinthroughsteam/sits_landing.png" alt="Steam Login"/></a>
+    </>
+  );
+}
+
+export default App;
