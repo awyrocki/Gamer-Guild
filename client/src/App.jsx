fe7160e58e2042ca18081189175b688a56384fbc
Austin Wyrocki <austinwyrocki@gmail.com>
2023-07-18 15:05:43 -0400

    guild list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav/Nav';
 import User from './components/User/User';
+import Guildlist from './components/Guildlist/Guildlist';
 
 function App() {
   return (
     <>
     <Nav />
     <User />
+    <Guildlist />
     </>
   );
 }
