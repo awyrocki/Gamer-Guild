20d34af37fa88e5dc72d26ccbf69efc0af4bd91b
Abusino Roziqov <jo2003king@gmail.com>
2023-07-19 07:15:34 -0600

    should work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css';
-import Login from './components/Login/Login';
-import Forgot from './components/Forgot/Forgot';
 function App() {
   return (
     <>
-    <Login/>
     </>
   );
 }
