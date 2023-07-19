35db9bcead4c5f78fd8ab06836d6b2343b12fafd
cyrus fullam <cyrusfullam@gmail.com>
2023-07-19 11:34:28 -0400

    fetch done

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Steam from '../Steam/Steam';
 import "./User.css"
 
-function User() {
+function User({ userID, steamID, isLinked }) {
+  const [ userProfile, setUserProfile ] = useState(null);
+  const [ render, setRender ] = useState(false)
+
+  const fetchUser = () => {
+      const url = `http://127.0.0.1:4000/user/${userId}`
+
+      fetch(url, {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application.json"
+      })
+    })
+    .then(res => res.json())
+    .then(data => setUserProfile(data))
+    .catch(err = console.log(err))
+  }
+
+  const fetchSteamUser = () => {
+    const url = `http://127.0.0.1:4000/user/${steamId}`
+    fetch(url, {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application.json"
+    })
+  })
+  .then(res => res.json())
+  .then(data => setUserProfile(data))
+  .catch(err = console.log(err))
+      
+  }
+
+  useEffect(() => {
+    if(!isLinked){
+      fetchUser()
+    } else {
+      fetchSteamUser()
+    }
+  }, [])
+
+  
   return (
     <div id='profile-container'>
+      {console.log(userProfile)}
         <div id='placeholder'>
             Place steam image here
         </div>
