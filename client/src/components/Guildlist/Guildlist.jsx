d8e8e5ebde39f4be5441ff8214e5237bb154c689
Austin Wyrocki <austinwyrocki@gmail.com>
2023-07-20 13:40:17 -0400

    guilds joined render

--- client/src/components/Guildlist/Guildlist.jsx
+++ client/src/components/Guildlist/Guildlist.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Guildlist.css"
 
 function Guildlist() {
+
+  const [ joinedGuilds, setJoinedGuilds ] = useState([])
+  // grabs the user id from the local storage
+  const userID = localStorage.getItem("id")
+
+  const fetchJoinedGuilds = () => {
+    const url = `http://127.0.0.1:4000/guild/${userID}`
+
+    fetch(url, {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        // ! authorization goes here!
+      })
+    })
+    .then(res => res.json())
+    .then(data => setJoinedGuilds(data))
+    .catch(err => console.log(err))
+  }
+  // grabs joined guilds when user logs in
+  useEffect(() => {
+    fetchJoinedGuilds()
+  })
+
   return (
     <div id='guildlist-container'>
-      <h3 id='guild-title'>Guilds</h3>
-      <h4 id='game-title'>Among Us</h4>
-      <span id='game-description'>A fun party game with friends</span>
-      <h4 id='game-title'>League of Legends</h4>
-      <span id='game-description'>Competitive MOBA</span>
-      <h4 id='game-title'>CS:GO</h4>
-      <span id='game-description'>Competitive FPS</span>
-      <h4 id='game-title'>Stardew Valley</h4>
-      <span id='game-description'>Cute farming sim</span>
+      {joinedGuilds.map((guild, i) => (
+        <div key={i} className='joined-guild-list'>
+          <h3>{guild.name}</h3>
+          <p>{guild.description}</p>
+        </div>
+      ))}
     </div>
   )
 }
