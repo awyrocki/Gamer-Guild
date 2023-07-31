32d66a45e6f937319eb967f7103a54dc269956ac
cyrus fullam <cyrusfullam@gmail.com>
2023-07-31 16:21:32 -0400

    profile pic

--- client/src/components/Steam/Steam.jsx
+++ client/src/components/Steam/Steam.jsx
@@ -7,6 +7,7 @@ function Steam({ userId }) {
     const token = localStorage.getItem("token")
     const [ redirectHome, setRedirectHome ] = useState(false)
     const [ steamId, setSteamId ] = useState("")
+    const [ profilePic, setProfilePic ] = useState("")
 
         //checks if there is a param
         useEffect(() => {
@@ -16,17 +17,43 @@ function Steam({ userId }) {
             }
         }, [])
 
+        // waits for profile pic and then adds it to users account
+        useEffect(() => {
+            if(profilePic !== "") {
+                linkSteam()
+            }
+
+        }, [profilePic])
+
         // initiate call for adding steamid to user
         useEffect(() => {
             if(steamId !== "") {
-                linkSteam()
+                getProfilePic()
             }
         },[steamId])
+
+
+        // retrieves users profile pic
+        function getProfilePic() {
+            const url = `http://localhost:4000/steamUser/${steamID}`
+            fetch(url, {
+                method: "GET",
+                headers: new Headers({
+                "Content-Type": "application/json"
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                setProfilePic(data.response.players[0].avatarfull)
+                
+            })
+        }
 
         // the actual call to add steam id to user
         function linkSteam() {
             const body = {
-                "steamId": steamId
+                "steamId": steamId,
+                "profilePic": profilePic
             };
                 const url = `http://localhost:4000/user/update/${userId}`
                     fetch(url, {
